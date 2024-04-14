e69bf7a4bfa065a8a8f37ee3403ab0d1cabefcd8
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-04-14 19:52:01 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { PostModule } from './post/post.module';
+import { SupabaseService } from './supabase/supabase.service';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      entities: ['dist/**/*.entity.js'],
+      database: process.env.TYPEORM_DATABASE,
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+    UsersModule,
+    PostModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    SupabaseService,
+  ],
+})
+export class AppModule {}
