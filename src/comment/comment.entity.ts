f69fd76cf348ba53d5a5cd2d3896589c5b79e012
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-04-30 13:27:42 -0300

    feat: add Comment Entity

---
+++ src/comment/comment.entity.ts
@@ -0,0 +1,33 @@
+import { Post } from 'src/post/post.entity';
+import { User } from 'src/users/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity({ name: 'comments' })
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => User, (user) => user.comments)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @ManyToOne(() => Post, (post) => post.comments)
+  @JoinColumn({ name: 'postId' })
+  post: Post;
+
+  @Column()
+  postId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
